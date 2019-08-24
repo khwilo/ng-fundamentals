332afbf4df0548f3d9cee4076e584f4ac2f8a886
Khwilo Kabaka <khwilowatai@gmail.com>
2019-08-24 09:23:15 +0300

    Add a basic shield image

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello World</h1>',
+  template: `
+    <h1>Hello World</h1>
+    <img src="/assets/images/basic-shield.png" />
+  `,
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
