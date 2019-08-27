f985d8a893b2b64ad12e1a15510da6928c71a558
Khwilo Kabaka <khwilowatai@gmail.com>
2019-08-27 07:24:32 +0300

    Add site header

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-events-list></app-events-list>'
+  template: `
+    <app-nav-bar></app-nav-bar>
+    <app-events-list></app-events-list>
+  `
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
