5457d3b82c161ddd522ce94199e89b170cd48612
Khwilo Kabaka <khwilowatai@gmail.com>
2019-08-27 10:59:13 +0300

    Render a list of events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -17,6 +17,7 @@ import { Component, Input } from '@angular/core';
     </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
