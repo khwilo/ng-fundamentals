1be60cb4f11049ca4e2b2ea4acf9ac9ed8a1aa63
Khwilo Kabaka <khwilowatai@gmail.com>
2019-08-29 14:58:32 +0300

    Add a route for the 404 page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventsListComponent } from './events/events-list.component';
 
@@ -8,5 +9,6 @@ export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventDetailsComponent },
+  { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 ];
